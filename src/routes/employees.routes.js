20b97d334db3c0989cd2cf8c78203f1313f29a44
Bruno Martinez <xavier.akr2021@gmail.com.mx>
2023-08-16 09:30:04 -0700

    first commit

---
+++ src/routes/employees.routes.js
@@ -0,0 +1,13 @@
+import { Router } from "express";
+import { getEmployee, getEmployees, createEmployees, updateEmployees, deleteEmployees } from "../controllers/employees.controllers.js";
+
+const router = Router()
+
+router.get('/employees', getEmployees)
+router.get('/employee/:id', getEmployee)
+router.post('/employees', createEmployees)
+router.put('/employees/:id',updateEmployees)
+router.delete('/employees/:id',deleteEmployees)
+
+
+export default router
