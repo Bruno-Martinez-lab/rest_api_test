f6ada877529618514562a270a0f2fc1f360ecec8
Bruno Martinez <bruno.mtz.vqz@gmail.com>
2023-08-16 10:57:02 -0700

    SOLUTION FAILURE IN ENDPONIT CONECTION

--- src/controllers/employees.controllers.js
+++ src/controllers/employees.controllers.js
@@ -47,10 +47,10 @@ export const createEmployees = async (req, res) => {
 export const updateEmployees = async(req, res) => {
     try {
         const {id} = req.params
-    const {name, salary} = req.body
+        const {name, salary} = req.body
 
-    const [result] = await pool.query('UPDATE employee SET name = IFNULL(?, name), salary = IFNULL(?, salary) WHERE id = ?', [name, salary, id])
-    if (result.affectedRows === 0) return res.status(404).json({
+        const [result] = await pool.query('UPDATE employee SET name = IFNULL(?, name), salary = IFNULL(?, salary) WHERE id = ?', [name, salary, id])
+     if (result.affectedRows === 0) return res.status(404).json({
         message: 'Employee not found'
     })
     
