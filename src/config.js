2dd983e36e5fc734e4c3f578360562cbc35c4893
Bruno Martinez <bruno.mtz.vqz@gmail.com>
2023-08-16 10:28:46 -0700

    SOLUTION FAILURE IN CONNECTION WITH DB

--- src/config.js
+++ src/config.js
@@ -8,5 +8,5 @@ export const DB_HOST = process.env.DB_HOST || 'localhost'
 export const DB_PORT = process.env.DB_PORT || 3606
 export const DB_USER = process.env.DBUSER || 'root'
 export const DB_PASSWORD = process.env.DB_PASSWORD || '0303'
-export const DB_DATABASE = process.env.companydb || 'companydb'
+export const DB_DATABASE = process.env.DB_DATABASE || 'companydb'
 
